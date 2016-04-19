645093b0798a96604541642a9d47336b3df571ac
anastasiashpak <anastasia.shpak@ymedialabs.com>
2016-04-19 07:57:18 -0700

    adding clean project

---
+++ src/test/java/companyname/AllTests.java
@@ -0,0 +1,11 @@
+package companyname;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SeleniumTest.class })
+
+public class AllTests {
+}
